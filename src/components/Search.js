9569044a095186d43b6ae580b657f20960ed1bd4
Sonja Parsell <sonjaparsell09@gmail.com>
2022-04-11 17:38:54 -0400

    wip - using api to search

---
+++ src/components/Search.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Search extends React.Component {
+
+        state = {term: ''};
+
+        handleChange = (e) => {
+            this.setState( {term: e.target.value} )
+            console.log(this.state)
+        }
+
+        onFormSubmit = (e) => {
+            e.preventDefault();
+            this.props.onTermSubmit(this.state.term)
+        }
+
+    render() {
+        return (
+            <div className="ui segment search-bar">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Crypto CCY Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.handleChange}
+                        />
+                    <button className="ui button">Search</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search;
