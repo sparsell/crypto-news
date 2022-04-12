873c52afa8012ba42b7e8d66c60ce3797fe3260a
Sonja Parsell <sonjaparsell09@gmail.com>
2022-04-12 11:51:57 -0400

    wip - api response and return

--- src/apis/coinranking.js
+++ src/apis/coinranking.js
@@ -3,42 +3,24 @@ import axios from 'axios';
 
 export const options = {
     method: 'GET',
-    // url: 'https://coinranking1.p.rapidapi.com/coins',
-    // params: {
-    //   referenceCurrencyUuid: 'yhjMzLPhuIDl',
-    //   timePeriod: '24h',
-    // },
-    // headers: {
-    //   'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    //   'X-RapidAPI-Key': '176800dfd3msh3af75817d22f472p1e9a6cjsn7be99f53cf95'
-    url: 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/exchanges',
+    url: 'https://coinranking1.p.rapidapi.com/coins',
     params: {
       referenceCurrencyUuid: 'yhjMzLPhuIDl',
-      limit: '50',
-      offset: '0',
-      orderBy: '24hVolume',
-      orderDirection: 'desc'
+      timePeriod: '24h',
     },
     headers: {
       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
       'X-RapidAPI-Key': '176800dfd3msh3af75817d22f472p1e9a6cjsn7be99f53cf95'
     }
-
-    //   url: 'https://coinranking1.p.rapidapi.com/search-suggestions',
-    //   params: {referenceCurrencyUuid: 'yhjMzLPhuIDl'},
-    //   headers: {
-    //     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    //     'X-RapidAPI-Key': '176800dfd3msh3af75817d22f472p1e9a6cjsn7be99f53cf95'
-    // }
   };
   
   export const getData = (options) => {
       axios.request(options).then(function (response) {
-      console.log(response.data);
+      let coinData = response.data.data.coins
+      console.log(coinData)
   }).catch(function (error) {
       console.error(error);
   })
-}
 
-// console.log(getData(options))
+}
   
